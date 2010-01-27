5c8a3c6ddecee54adfc972a72685f027f91a5f16
Wesley Moore <wez@wezm.net>
2010-01-27 21:17:02 +1100

    Add prettyPhoto pop-up for photos

--- output/js/home.js
+++ output/js/home.js
@@ -6,7 +6,8 @@ jQuery(function () {
     jQuery.each(data.photos.photo, function(i, photo) {
       var li = $("<li></li>");
       var a = $("<a></a>");
-      a.attr("href", "http://www.flickr.com/photos/" + escape(photo.owner) + "/" + photo.id);
+      a.attr("href", photo.url_m);
+      a.attr("rel", "prettyPhoto[flickr]");
       var img = $("<img />");
       img.attr('src', photo.url_sq);
       img.attr('alt', photo.title);
@@ -14,8 +15,9 @@ jQuery(function () {
       li.append(a);
       ul.append(li);
     });
+    $("a[rel^='prettyPhoto']").prettyPhoto({theme: "facebook"});
   };
 
   // Populate Flickr
-  jQuery.getJSON("http://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=aa003631cc50bd47f27f242d30bcd22f&user_id=40215689%40N00&format=json&per_page=20&extras=url_sq&jsoncallback=?", populate_flickr);
+  jQuery.getJSON("http://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=aa003631cc50bd47f27f242d30bcd22f&user_id=40215689%40N00&format=json&per_page=20&extras=url_sq,url_m&jsoncallback=?", populate_flickr);
 });
