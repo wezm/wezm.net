b98a36817cf8d8b121c73ce422a7eebea93698e8
Wesley Moore <wez@wezm.net>
2010-08-18 20:28:23 +1000

    Fix lightbox on homepage

--- output/js/home.js
+++ output/js/home.js
@@ -20,6 +20,7 @@ jQuery(function () {
       li.css("background-position", (i * -75) + 'px -75px');
       ul.append(li);
     });
+    $("a[rel^='prettyPhoto']").prettyPhoto({theme: "facebook"});
   };
 
   // Populate Flickr
